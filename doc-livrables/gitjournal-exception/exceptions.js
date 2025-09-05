85cb01d1e612211409e779393f053c1321b69946
samumuku <pr13xpj@eduvaud.ch>
2025-09-05 14:40:10 +0200

    doc(rapport): création rapport + ajout exceptions.js
    [45][DONE]
    J'ai ajouté le fichier exceptions.js, et j'ai crée le Rapport et ajouté les informations nécessaires de base, comme le domaine d'application

---
+++ doc-livrables/gitjournal-exception/exceptions.js
@@ -0,0 +1,11 @@
+const exceptions = [
+  {
+    "patch": true,
+    "name": "Absent",
+    "description": "J'étais absent tout l'après-midi car j'avais un entretien pour le stage début 2026.",
+    "date": "2025-08-29T12:09:00.000Z",
+    "duration": 200,
+    "status": "Done",
+    "author": "samumuku"
+  }
+]
