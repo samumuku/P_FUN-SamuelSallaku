03ce3fd53eaf28d22bf4d64e3d3707d91095188e
samumuku <pr13xpj@eduvaud.ch>
2025-10-03 16:34:18 +0200

    add exceptions

--- doc-livrables/gitjournal-exception/exceptions.js
+++ doc-livrables/gitjournal-exception/exceptions.js
@@ -16,5 +16,14 @@ const exceptions = [
     "duration": 90,
     "status": "Done",
     "author": "samumuku"
+  },
+  {
+    "patch": true,
+    "name": "feat(app): add year list",
+    "description": "Je n'arrive pas à régler le problème où je ne peux pas ajouter les années sur une des list box",
+    "date": "2025-10-03T14:32:00.000Z",
+    "duration": 40,
+    "status": "WIP",
+    "author": "samumuku"
   }
 ]
