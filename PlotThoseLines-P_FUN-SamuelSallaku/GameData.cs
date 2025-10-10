d18c173cb291c20a00dd5f85cf3cb6d5c0cdd43c
samumuku <pr13xpj@eduvaud.ch>
2025-10-10 14:34:49 +0200

    feat(repo): add separate gamedata class
    [5][DONE]

---
+++ PlotThoseLines-P_FUN-SamuelSallaku/GameData.cs
@@ -0,0 +1,10 @@
+﻿namespace PlotThoseLines_P_FUN_SamuelSallaku
+{
+    // classe des données
+    public class GameData
+    {
+        public string Name;
+        public int Year;
+        public double Sales;
+    }
+}
