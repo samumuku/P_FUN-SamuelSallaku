ab04decd442651be19cbbcd39e5c9a8633ce037d
samumuku <pr13xpj@eduvaud.ch>
2025-10-31 13:39:10 +0100

    feat(database): add database connection for data
    [30][WIP]
    J'ai ajouté la librairie microsoft.data.sqlite pour une meilleure persistence de données qu'un simple fichier txt mais il n'est pas complet car j'ai fait seulement la connexion à la db et la création de la table

---
+++ PlotThoseLines-P_FUN-SamuelSallaku/GameDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace PlotThoseLines_P_FUN_SamuelSallaku
+{
+    internal class GameDatabase
+    {
+        private static string connectionDb = "Data Source=games.db";   // indiquer la source de la DB
+
+        //initializer la DB
+        public static void initializeDb()
+        {
+            // connexion à la db
+            using var connection = new SqliteConnection(connectionDb);
+            connection.Open();
+
+            // spécifications de la table
+            string table = @"
+            CREATE TABLE IF NOT EXISTS t_games (
+                Name TEXT NOT NULL,
+                Year INTEGER NOT NULL,
+                Sales REAL NOT NULL
+            );";
+
+            //création de la table t_games en spécifiant la commande et connexion
+            using var createTable = new SqliteCommand(table, connection);
+
+            //permet d'écrire les données dans la table
+            createTable.ExecuteNonQuery();
+        }
+    }
+}
